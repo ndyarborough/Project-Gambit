68de78bc2923916990fd19d71fe9422fb21108ce
ndyarborough <ndyarborough1997@gmail.com>
2017-11-13 22:40:25 -0500

    added page components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './pages/Home';
+import Register from './pages/Register';
 
 const App = () =>
   <Router>
     <div>
-        <Route exact path="/" component={Home} />
+        <Switch>  
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
     </div>
   </Router>;
 
