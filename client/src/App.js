d42aac9ce43568ad0d84c7b77212d746066e6688
Noah Yarborough <ndyarborough1997@gmail.com>
2017-11-15 01:50:55 -0500

    login and register components added as paths on home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/Home';
 import Register from './pages/Register';
+import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 
 const App = () =>
@@ -10,6 +11,7 @@ const App = () =>
         <Switch>  
           <Route exact path="/" component={Home} />
           <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
           <Route component={NoMatch} />
         </Switch>
     </div>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
 import './Home.css';
 
 const Home = () => {
-    return(
+    return (
         <div>
-            <h1>Home Page</h1>
+            <Link to='/login'>
+                <Button>Login</Button>
+            </Link>
+            or 
+            <Link to='/register'>
+                <Button>Register</Button>
+            </Link>    
         </div>
-    )
+    );
 }
 
 export default Home;
