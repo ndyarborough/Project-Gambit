f9559673c5a326dd61df2383e46f4b13d375c12a
Nicholas <nicholasallenjr@gmail.com>
2017-11-19 22:48:08 -0500

    working backend for register and login with user stats

--- controllers/router.js
+++ controllers/router.js
@@ -35,14 +35,23 @@ router.route('/register').post(function(req, res) {
 })
 
 router.route('/login/:email/:password').get(function(req, res) {
-  console.log(req.params)
   User.findOne({
     'email': req.params.email
   }).exec(function(err, doc) {
-    console.log(doc);
-    request(`https://masteroverwatch.com/profile/${doc.platform}/${doc.region}/${doc.gamerTag}`, function(err, response, html) {
+    request(`https://masteroverwatch.com/profile/${doc.platform}/${doc.region}/${doc.gamerTag}/season/7`, function(err, response, html) {
+      
       const $ = cheerio.load(html);
 
+      // if ($('ul.player-navigation-extras').children('li.navigation-toggle').children('div:nth-child(1)').children('a').attr('class') == 'toggle-button btn btn-default is-active') {
+      //   console.log('its active')
+      //   app.post('http://masteroverwatch.com/profile/mode/toggle', {
+      //     mode: 'ranked'
+      //   }).then(function(req, res) {
+      //     console.log(posted);
+      //   })
+      // }else {
+      //   console.log('worked');
+      // }
       function replaceNan(data) {
         if (isNaN(data)) {
           return 0;
@@ -51,13 +60,14 @@ router.route('/login/:email/:password').get(function(req, res) {
         }
       }
 
+
       // Scrape Lifetime Stats
-      const userResults = {};
+      const userResults = {}; 
 
       const lifetimeStats = $('div.data-overall').children('div:nth-child(2)');
 
-      userResults.gamesPlayed = replaceNan(parseFloat(lifetimeStats.children('div:nth-child(2)').children('div:nth-child(1)').children('div:nth-child(2)').children().first().text()));
-      userResults.wins = replaceNan(parseFloat(lifetimeStats.children('div:nth-child(2)').children('div:nth-child(2)').children('div:nth-child(2)').children().first().text()));
+      userResults.gamesPlayed = replaceNan(parseFloat(lifetimeStats.children('div:nth-child(2)').children('div:nth-child(1)').children('div:nth-child(2)').children().first().text().replace(/,/g,'')));
+      userResults.wins = replaceNan(parseFloat(lifetimeStats.children('div:nth-child(2)').children('div:nth-child(2)').children('div:nth-child(2)').children().first().text().replace(/,/g,'')));
       userResults.eliminations = replaceNan(parseFloat(lifetimeStats.children('div:nth-child(4)').children('div:nth-child(1)').children('div:nth-child(2)').children().first().text().replace(/,/g,'')));
       userResults.deaths = replaceNan(parseFloat(lifetimeStats.children('div:nth-child(4)').children('div:nth-child(2)').children('div:nth-child(2)').children().first().text().replace(/,/g,'')));
       userResults.kdr = replaceNan(parseFloat(lifetimeStats.children('div:nth-child(4)').children('div:nth-child(3)').children('div:nth-child(2)').children().first().text()));
@@ -65,8 +75,24 @@ router.route('/login/:email/:password').get(function(req, res) {
       userResults.damage = replaceNan(parseFloat(lifetimeStats.children('div:nth-child(4)').children('div:nth-child(5)').children('div:nth-child(2)').children().first().text().replace(/,/g,'')));
       userResults.healing = replaceNan(parseFloat(lifetimeStats.children('div:nth-child(4)').children('div:nth-child(7)').children('div:nth-child(2)').children().first().text().replace(/,/g,'')));
 
-      console.log(userResults);
-
+      User.findOneAndUpdate({
+        '_id': doc._id
+      }, {
+        'gamesPlayed': userResults.gamesPlayed,
+        'wins': userResults.wins,
+        'eliminations': userResults.eliminations,
+        'deaths': userResults.deaths,
+        'kdr': userResults.kdr,
+        'accuracy': userResults.accuracy,
+        'damage': userResults.damage,
+        'healing': userResults.healing
+      }).exec(function(erro, docu) {
+        if (erro) {
+          console.log(erro);
+        }else { 
+          //console.log(docu);
+        }
+      });
 
       // Scrape Stats for each hero
       const heroResults = {};
@@ -83,16 +109,25 @@ router.route('/login/:email/:password').get(function(req, res) {
         heroResults.objKills = replaceNan(parseFloat($(element).children('div:nth-child(3)').children('div:nth-child(2)').children('div:nth-child(1)').children('div:nth-child(1)').text().replace('/min', '')));
         heroResults.objTime = replaceNan(parseFloat($(element).children('div:nth-child(3)').children('div:nth-child(3)').children('div:nth-child(1)').children('div:nth-child(1)').text().replace(' seconds', '')));
 
-        console.log(heroResults);
-        // var heroEntry = new Hero(heroResults);
+        var heroEntry = new Hero(heroResults);
     
-        // heroEntry.save(function(err, doc) {
-        //   if (err) {
-        //     console.log(err);
-        //   }else {
-        //     console.log(heroResults);
-        //  }
-        //});
+        heroEntry.save(function(error, docum) {
+          if (error) {
+            console.log(error);
+          }else {
+            Hero.findOneAndUpdate(
+              {'_id':docum._id}, 
+              {'User': doc._id}
+            ).exec(function(errors, docume) {
+              if (errors) {
+                console.log(errors);
+              }else {
+                //no response
+              }
+            })
+            //no response
+         }
+        });
       });
     })
   })
