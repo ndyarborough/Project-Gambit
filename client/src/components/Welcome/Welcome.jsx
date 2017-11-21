32b1bf566cfe261e6ea2ef92f4cec63dfbbac2d1
Noah Yarborough <ndyarborough1997@gmail.com>
2017-11-21 18:52:04 -0500

    styles

--- client/src/components/Welcome/Welcome.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Welcome = () => {
-    <div>Hello</div>
-}
-
-export default Welcome;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Home.css';
-import Welcome from '../../components/Welcome/Welcome.jsx';
+import charactersImage from '../../imgs/overwatch-cast.jpg.optimal.jpg';
+import { Link } from 'react-router-dom';
 
 const Home = () => 
         <div id='home'>
-           
+                <div id='welcomeBox'>
+                        <h1>Pick your comp hero before the game starts!</h1>
+                        <hr />
+                        <div id='characterPic'></div>
+                        <Link to='/register'>Create my Account and get started</Link>
+                </div>
         </div>
 
 export default Home;
