5a6e9c8538e26a3344a91ef577eaab62735c0802
Noah Yarborough <ndyarborough1997@gmail.com>
2017-12-09 21:54:46 -0500

    hero.js dissapeared -- replacing here

---
+++ models/hero.js
@@ -0,0 +1,56 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var HeroSchema = new Schema({
+  name: {
+    type: String
+  },
+
+  eliminations: {
+    type: Number
+  },
+
+  hoursPlayed: {
+    type: String
+  },
+
+  kdr: {
+    type: Number
+  },
+
+  accuracy: {
+    type: Number
+  },
+
+  wins: {
+    type: Number
+  },
+
+  healing: {
+    type: Number
+  },
+
+  damage: {
+    type: Number
+  },
+
+  objKills: {
+    type: Number
+  },
+
+  objTime: {
+    type: Number
+  },
+
+  // This only saves one note's ObjectId, ref refers to the Note model
+  User: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+},
+{
+  timestamps: true
+});
+
+var Hero = mongoose.model("Hero", HeroSchema);
